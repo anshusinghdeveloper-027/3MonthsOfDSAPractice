e422745831b16ac7c3c6def2c86181a8da717981
anshu-home <developer.anshusingh@gmail.com>
2025-03-17 23:58:08 +0530

    divide array

---
+++ src/array/DivideArray.java
@@ -0,0 +1,25 @@
+package array;
+
+public class DivideArray {
+
+    public static void main(String[] args) {
+        int []nums={3,2,3,2,2,2};
+        System.out.println("Result :"+divideArray(nums));
+
+    }
+
+    public  static  boolean divideArray(int[] nums) {
+        int arr[]=new int[501];
+        for(int i=0;i<nums.length;i++){
+            arr[nums[i]]++;
+        }
+        boolean flag=true;
+        for(int i=0;i<501;i++){
+            if(arr[i]%2!=0){
+                return false;
+            }
+        }
+        return flag;
+
+    }
+}
